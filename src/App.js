f139fc62ac0024c8b81929a32fd187b66adde5ef
akim-dev <musta.akim@gmail.com>
2022-03-07 23:32:23 +0700

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import FeedbackItem from './components/FeedbackItem';
 
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Header text="Feedback UI" />
-      <FeedbackItem/>
+      <FeedbackItem />
     </div>
   );
 }
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -2,9 +2,21 @@ import React from 'react';
 import { useState } from 'react';
 
 function FeedbackItem() {
+  const [rating, setRating] = useState(10);
+  const [text, setText] = useState('This is default text from useState');
+
+  const hadleChangeRating = function () {
+    setRating(function (prev) {
+      console.log(prev);
+      return prev + 1;
+    });
+  };
+
   return (
     <div className="card">
-      <h3>Feedback Item</h3>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
+      <button onClick={hadleChangeRating}>Click Me</button>
     </div>
   );
 }
